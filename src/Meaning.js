2ab200ff74981804162ddaf06c4fe46706128f60
uncanny-vegan-taco <kylihumphreys@gmail.com>
2023-11-20 12:10:22 -0800

    Added Synonym Component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -12,6 +13,8 @@ export default function Meaning(props) {
               <br />
               <em>{definition.example}</em>
             </p>
+            <br />
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}

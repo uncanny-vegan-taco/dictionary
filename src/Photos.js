f93172e6b8b6efead87757a75a1a1cc2cd21d774
uncanny-vegan-taco <kylihumphreys@gmail.com>
2023-11-21 09:36:57 -0800

    Added an alt because I have nothing to lose

--- src/Photos.js
+++ src/Photos.js
@@ -8,7 +8,12 @@ export default function Photos(props) {
         {props.photos.map(function (photo, index) {
           return (
             <div key={index}>
-              <a href={photo.src.original} target="_blank" rel="noreferrer">
+              <a
+                href={photo.src.original}
+                target="_blank"
+                rel="noreferrer"
+                alt="Photo"
+              >
                 <img src={photo.src.portrait} />
               </a>
             </div>
